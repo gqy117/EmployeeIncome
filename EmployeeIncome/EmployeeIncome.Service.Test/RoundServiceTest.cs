9860a471d5da78fb16f1bcae25053796313ce87a
Leo Li <gqy117@gmail.com>
2015-09-22 21:27:23 +1200

    Added more cases for RoundServiceTest.cs.

--- EmployeeIncome/EmployeeIncome.Service.Test/RoundServiceTest.cs
+++ EmployeeIncome/EmployeeIncome.Service.Test/RoundServiceTest.cs
@@ -20,30 +20,28 @@
         }
 
         [Test]
-        public void Round_ShouldReturn3_WhenInputIs2Point5()
+        [TestCase(0.5)]
+        [TestCase(0.9)]
+        public void Round_ShouldReturn1_WhenInputIsGreaterThan0Point5(double input)
         {
-            // Arrange
-            double input = 2.5;
-
             // Act
             int actual = this.roundService.Round(input);
 
             // Assert
-            int expected = 3;
+            int expected = 1;
             actual.ShouldBeEquivalentTo(expected);
         }
 
         [Test]
-        public void Round_ShouldReturn2_WhenInputIs2Point4()
+        [TestCase(0.4999)]
+        [TestCase(0)]
+        public void Round_ShouldReturn2_WhenInputIsIsLessThan0Point5(double input)
         {
-            // Arrange
-            double input = 2.4;
-
             // Act
             int actual = this.roundService.Round(input);
 
             // Assert
-            int expected = 2;
+            int expected = 0;
             actual.ShouldBeEquivalentTo(expected);
         }
     }
